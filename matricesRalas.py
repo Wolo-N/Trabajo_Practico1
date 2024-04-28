aa9f01ac141733a195746f9ef875e7503801509d
Wolo-N <nicolas.wolodarskyfranke@gmail.com>
2024-04-28 19:29:36 -0300

    emprolijasao

--- matricesRalas.py
+++ matricesRalas.py
@@ -1,18 +1,17 @@
 # IMPORTANTE: Para importar estas clases en otro archivo (que se encuentre en la misma carpeta), escribir:
-# from matricesRalas import MatrizRala, GaussJordan 
+# from matricesRalas import MatrizRala, GaussJordan
 
 class ListaEnlazada:
     def __init__( self ):
         self.raiz = None
         self.longitud = 0
-        
         self.current = self.Nodo(None, self.raiz)
- 
+
     def insertarFrente( self, valor ):
         # Inserta un elemento al inicio de la lista
         if len(self) == 0:
-            return self.push(valor)    
-    
+            return self.push(valor)
+
         nuevoNodo = self.Nodo( valor, self.raiz )
         self.raiz = nuevoNodo
         self.longitud += 1
@@ -31,14 +30,14 @@ class ListaEnlazada:
         # Inserta un elemento al final de la lista
         if self.longitud == 0:
             self.raiz = self.Nodo( valor, None )
-        else:      
+        else:
             nuevoNodo = self.Nodo( valor, None )
             ultimoNodo = self.nodoPorCondicion( lambda n: n.siguiente is None )
             ultimoNodo.siguiente = nuevoNodo
 
         self.longitud += 1
         return self
-    
+
     def pop( self ):
         # Elimina el ultimo elemento de la lista
         if len(self) == 0:
@@ -48,7 +47,7 @@ class ListaEnlazada:
         else:
             anteUltimoNodo = self.nodoPorCondicion( lambda n: n.siguiente.siguiente is None )
             anteUltimoNodo.siguiente = None
-        
+
         self.longitud -= 1
 
         return self
@@ -57,15 +56,15 @@ class ListaEnlazada:
         # Devuelve el primer nodo que satisface la funcion "funcionCondicion"
         if self.longitud == 0:
             raise IndexError('No hay nodos en la lista')
-        
+
         nodoActual = self.raiz
         while not funcionCondicion( nodoActual ):
             nodoActual = nodoActual.siguiente
             if nodoActual is None:
                 raise ValueError('Ningun nodo en la lista satisface la condicion')
-            
+
         return nodoActual
-        
+
     def __len__( self ):
         return self.longitud
 
@@ -79,7 +78,7 @@ class ListaEnlazada:
         else:
             self.current = self.current.siguiente
             return self.current.valor
-    
+
     def __repr__( self ):
         res = 'ListaEnlazada([ '
 
@@ -101,17 +100,6 @@ class MatrizRala:
         self.filas = {}
         self.shape = (M, N)
 
-    """def __getitem__(self, Idx):
-    # Esta función implementa la indexación (Idx es una tupla (m,n)) -> A[m,n]
-    # tupla m = filas y n = columnas
-        m, c = Idx
-        if m in self.filas:
-            fila = self.filas[m]
-            actual = fila.nodoPorCondicion(lambda n: n.valor[0] == c) # 
-            return actual.valor[1]
-        else:
-            return 0"""
-        
     def __getitem__( self, Idx ):
         # Esta funcion implementa la indexacion ( Idx es una tupla (m,n) ) -> A[m,n]
         # tupla m= filas y n = columnas
@@ -124,7 +112,7 @@ class MatrizRala:
                     return actual.valor[1]
                 actual = actual.siguiente
         return 0
-        
+
     def __setitem__(self, Idx, v):
         # Esta función implementa la asignación durante indexación (Idx es una tupla (m,n)) -> A[m,n] = v
         m, c = Idx # m filas, c columnas
@@ -136,7 +124,7 @@ class MatrizRala:
                 if actual.valor[0] == c:
                     # Si la columna ya existe en la fila, actualizamos el valor
                     actual.valor = (c, v)
-                    return 
+                    return
                 elif actual.valor[0] > c:
                     # Si encontramos una columna mayor que la que buscamos, insertamos el nuevo valor antes
                     nuevo_nodo = ListaEnlazada.Nodo((c, v), actual)
@@ -145,7 +133,7 @@ class MatrizRala:
                     else:
                         anterior.siguiente = nuevo_nodo
                         nuevo_nodo.siguiente = actual  # Actualizamos el puntero siguiente del nuevo nodo
-                    return 
+                    return
                 anterior = actual
                 actual = actual.siguiente
             # Si la columna no existe en la fila, la insertamos al final de la fila
@@ -169,7 +157,6 @@ class MatrizRala:
                     actual = actual.siguiente
         return matriz_resultado
 
-    
     def __rmul__( self, k ):
         # Esta funcion implementa el producto escalar-matriz -> k * A
         return self * k
@@ -178,10 +165,10 @@ class MatrizRala:
         # Verifica que las matrices tengan la misma forma
         if self.shape != other.shape:
             raise ValueError("Las matrices deben tener la misma forma para ser sumadas.")
-        
+
         # Crea una matriz resultante con los mismos tamaños que las matrices de entrada
         result = MatrizRala(self.shape[0], self.shape[1])
-        
+
         # Itera sobre las filas
         for i in self.filas:
             # Verifica si la fila i está presente en other
@@ -219,7 +206,7 @@ class MatrizRala:
                 while current_self is not None:
                     result[i, current_self.valor[0]] = current_self.valor[1]
                     current_self = current_self.siguiente
-        
+
         # Copia los elementos de other que no están presentes en self
         for i in other.filas:
             if i not in self.filas:
@@ -227,17 +214,17 @@ class MatrizRala:
                 while current_other is not None:
                     result[i, current_other.valor[0]] = current_other.valor[1]
                     current_other = current_other.siguiente
-                    
+
         return result
 
     def __sub__(self, other):
         # Verifica que las matrices tengan la misma forma
         if self.shape != other.shape:
             raise ValueError("Las matrices deben tener la misma forma para ser restadas.")
-        
+
         # Crea una matriz resultante inicializada como una matriz rala de la misma forma que las matrices de entrada
         result = MatrizRala(self.shape[0], self.shape[1])
-        
+
         # Itera sobre las filas de self
         for i in self.filas:
             # Verifica si la fila i está presente en other
@@ -275,7 +262,7 @@ class MatrizRala:
                 while current_self is not None:
                     result[i, current_self.valor[0]] = current_self.valor[1]
                     current_self = current_self.siguiente
-        
+
         # Copia los elementos de other que no están presentes en self con signo negativo
         for i in other.filas:
             if i not in self.filas:
@@ -283,10 +270,10 @@ class MatrizRala:
                 while current_other is not None:
                     result[i, current_other.valor[0]] = -current_other.valor[1]
                     current_other = current_other.siguiente
-                    
+
         return result
 
-    
+
     def __matmul__( self, other ):
         # COMPLETAR:
         # Esta funcion implementa el producto matricial (notado en Python con el operador "@" ) -> A @ B
@@ -317,7 +304,7 @@ class MatrizRala:
             res += '    [ '
             for j in range( self.shape[1] ):
                 res += str(self[i,j]) + ' '
-            
+
             res += ']\n'
 
         res += '])'
@@ -393,10 +380,3 @@ def multiplicar_matriz_vector(matriz, vector):
         resultado[i] = suma
     return resultado
 
-
-def main():
-    a = MatrizRala(3,3)
-    print(a)
-    
-if __name__ == "__main__":
-    main()
